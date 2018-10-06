256a727c667be30d39aed27cc25907f5ea5cf537
Avinash <avinash975@gmail.com>
2018-10-06 16:37:40 +0530

    Update rasiefund.sol

--- rasiefund.sol
+++ rasiefund.sol
@@ -7,11 +7,6 @@ contract RaiseFundsForACause {
     
     uint256  totalAmountRaised;
     
-    mapping(address => uint256) donationData;
-    mapping (address => uint256) balanceOf;
-
-    
-    
     struct projectDet  {
         
         string projectTitle;
@@ -19,40 +14,46 @@ contract RaiseFundsForACause {
         string user;
         uint256 dateOfPost;
         uint256 fundGoal;
+        uint256 totalAmountRaised;
         string  status;
         
     }
     
     mapping (uint => address) public projectToOwner;
+    mapping (address => mapping(uint => bool)) ifDonated;
     uint [] public projectIdList;
     projectDet[]  public projectDetails;
     event projAdded(uint id);
-    event transferred(uint amount , address receiver );
+    event transferred(uint amount , uint id );
 
-  
+/* This function will add the entries of project */
     function addProject(string _title ,string _description ,string _user , uint256 _dateOfPost , uint256 _fundGoal) public {
         
-        uint id = projectDetails.push(projectDet(_title, _description, _user, _dateOfPost, _fundGoal, "open" )) -1;
+        uint id = projectDetails.push(projectDet(_title, _description, _user, _dateOfPost, _fundGoal, 0,  "open" )) -1;
         projectIdList.push(id);
         projectToOwner[id] = msg.sender;
         
         emit projAdded(id);
     }
-
+    
+/* This will return the count of total projects entered */
     function getCount() public view returns(uint) {
         return projectIdList.length;
     }
     
-    function brosweproject(uint _id) public view returns(string , string , uint256 , uint256 , string , address) {
-       return (projectDetails[_id].projectTitle , projectDetails[_id].projectDescription ,projectDetails[_id].dateOfPost , projectDetails[_id].fundGoal , projectDetails[_id].status , projectToOwner[_id]); 
+/* This function will return the details of project for project id */
+    function brosweproject(uint _id) public view returns(string , string , uint256 , uint256 , string , address , uint256) {
+       return (projectDetails[_id].projectTitle , projectDetails[_id].projectDescription ,projectDetails[_id].dateOfPost , projectDetails[_id].fundGoal , projectDetails[_id].status , projectToOwner[_id] , projectDetails[_id].totalAmountRaised); 
   
     }
-    
+
+/* This function will return the list of all projects */    
    
    function getAll(uint _id) public view returns(uint , string) {
          return (projectIdList[_id] , projectDetails[_id].projectTitle);
    }
-    
+
+/* This function will return the list of all open projects */        
     function getOpen(uint _id) public view returns(uint , string ) {
          
            if (uint256(keccak256(projectDetails[_id].status))  == uint256(keccak256("open"))) {
@@ -60,25 +61,41 @@ contract RaiseFundsForACause {
            }
     
     }
-    
+
+
+/* This function will return the list of all closed projects */        
     function getClosed(uint _id) public view returns(uint , string) {
         if (uint256(keccak256(projectDetails[_id].status))  == uint256(keccak256("closed"))) {
            return (projectIdList[_id] , projectDetails[_id].projectTitle);
            }
    }
+
+
+/* This function will set the amount raised for any particular project */           
+   function setAmountRaised(uint _id, uint256 _amount) public  {
+       projectDetails[_id].totalAmountRaised += _amount;
+       ifDonated[msg.sender][_id] = true;
+       if (projectDetails[_id].totalAmountRaised >= projectDetails[_id].fundGoal)
+       {
+           setStatus(_id);
+       }
+       emit transferred(_amount , _id );
+   }
    
-   function setStatus(uint id) {
-       
+   function getAmoutRaised(uint _id) public view returns(uint256){
+       return(projectDetails[_id].totalAmountRaised);
+   }
+ 
+/* This function will return the status of project */          
+   function getStatus(uint _id) public view returns(string , bool){
+       return(projectDetails[_id].status , ifDonated[msg.sender][_id]);
+   }
+ 
+
+/* This function will be called and set the status to closed if fund goal reached */           
+   function setStatus(uint _id) public {
+       projectDetails[_id].status = 'closed';
    }
     
-    function transfer(address _receiever, uint256 _amount) public payable  {
-       // require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
-      //  balanceOf[msg.sender] -= _value;                    // Subtract from the sender
-      //  balanceOf[_to] += _value;                           // Add the same to the recipient
-      if(msg.sender.balance > _amount){
-       _receiever.transfer(_amount);
-     }
-      emit transferred(_amount , _receiever );
-    }
 
 }
